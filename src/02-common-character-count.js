57fe9ad833375fa9774516891df1324aec240b76
Valeriya Dudoits <lerk1512@gmail.com>
2021-04-09 22:54:19 +0300

    fix: fix bugs

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,21 +9,21 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount( s1, s2 ) {
-  let strArray1 = s1.split('');
-  let strArray2 = s2.split('');
-  let result = [];
+function getCommonCharacterCount(s1, s2) {
+  const strArray1 = s1.split('');
+  const strArray2 = s2.split('');
+  const result = [];
 
-  for (let j = 0; j <strArray1.length; j++) {
-      for (let i = 0; i < strArray2.length; i++) {
-          if (strArray1[j] === strArray2[i]) {
-              result.push(strArray1[j]);
-              strArray2.splice(i, 1);
-              strArray1.splice(j, 1);
-              j--;
-              console.log(strArray2);
-          }
+  for (let j = 0; j < strArray1.length; j++) {
+    for (let i = 0; i < strArray2.length; i++) {
+      if (strArray1[j] === strArray2[i]) {
+        result.push(strArray1[j]);
+        strArray2.splice(i, 1);
+        strArray1.splice(j, 1);
+        j--;
+        console.log(strArray2);
       }
+    }
   }
 
   return result.length;
